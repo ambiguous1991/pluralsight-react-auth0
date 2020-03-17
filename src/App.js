db666084dd59729435a248ac58129d47789346f6
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-03-17 13:42:15 +0100

    DoLogin

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
+import Auth from "./auth/Auth";
 
-function App() {
+function App({ history }) {
+  const auth = new Auth(history);
+
   return (
     <>
       <Nav />
       <div className="body">
-        <Route path="/" exact component={Home} />;
+        <Route
+          path="/"
+          exact
+          render={props => <Home auth={auth} {...[props]} />}
+        />
         <Route path="/profile" exact component={Profile} />
       </div>
     </>
